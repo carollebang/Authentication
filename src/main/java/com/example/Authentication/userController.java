d82228a3989be5fafe85a4b1e6d8b28fea346cad
carol <tlebang021@student.wethinkcode.co.za>
2022-10-18 15:31:17 +0200

    initial commit

---
+++ src/main/java/com/example/Authentication/userController.java
@@ -0,0 +1,12 @@
+package com.example.Authentication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class userController {
+    @RequestMapping("/getname")
+    public String   getUserName() {
+        return"welcome user ,soon will be requesting your details";
+    }
+}
